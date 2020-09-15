626f647c931d4d0af2a8d997a54e26d0401bff5e
Vincent Weltje <vincent.weltje@weareyou.com>
2020-09-15 14:27:05 +0200

    Populate shape dropdown

--- src/shapes/Controller.java
+++ src/shapes/Controller.java
@@ -1,4 +1,35 @@
 package shapes;
 
-public class Controller {
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    String[] shapes = {"Cube", "Cylinder", "Sphere", "Cone"};
+
+    @FXML
+    private ComboBox<String> shapeOptions;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.initShapeOptions();
+    }
+
+    private void initShapeOptions() {
+        shapeOptions.getItems().setAll(this.shapes);
+        shapeOptions.getSelectionModel().selectedItemProperty().addListener((selected, oldValue, newValue) -> handleShapeChange(newValue));
+    }
+
+    private void handleShapeChange(String newValue) {
+        System.out.println(newValue);
+    }
 }
--- src/shapes/Main.java
+++ src/shapes/Main.java
@@ -1,9 +1,15 @@
 package shapes;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
 public class Main extends Application {
