d3d157ee1c01a581c7a33719541f73608c34fff3
Vincent Weltje <vincent.weltje@weareyou.com>
2020-09-19 13:34:57 +0200

    Add event handlers for shapes screen

--- src/main/java/nl/avd/javadev/proftaak/PrimaryController.java
+++ src/main/java/nl/avd/javadev/proftaak/PrimaryController.java
@@ -1,12 +1,21 @@
 package nl.avd.javadev.proftaak;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PrimaryController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class PrimaryController implements Initializable {
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("feest");
     }
 }
