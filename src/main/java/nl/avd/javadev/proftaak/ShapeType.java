c78afc9be6a93820e56f1273cf8bfab833efb59e
Vincent Weltje <vincent.weltje@weareyou.com>
2020-10-03 15:08:11 +0200

    Create shapeType enum

---
+++ src/main/java/nl/avd/javadev/proftaak/ShapeType.java
@@ -0,0 +1,14 @@
+package nl.avd.javadev.proftaak;
+
+import java.util.Arrays;
+
+public enum ShapeType {
+    CONE,
+    CUBE,
+    CYLINDER,
+    PRISM,
+    PYRAMID,
+    SPHERE;
+
+    public static String[] shapeNames = {"Cone", "Cube", "Cylinder", "Prism", "Pyramid", "Sphere"};
+}
