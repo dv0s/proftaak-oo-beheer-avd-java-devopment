4c870eafa6697489afd298d9bda28987a7a5b61e
Vincent Weltje <vincent.weltje@weareyou.com>
2020-10-03 15:08:24 +0200

    Create extendable shape

---
+++ src/main/java/nl/avd/javadev/proftaak/Shape.java
@@ -0,0 +1,31 @@
+package nl.avd.javadev.proftaak;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Shape implements Initializable {
+
+    public ShapeType shapeType;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Cone");
+    }
+
+    @FXML
+    private void saveShape(ActionEvent event) {
+        System.out.println("TODO: Save input to database");
+    }
+
+    @FXML
+    private void discardChanges(ActionEvent event) throws IOException {
+        System.out.println("TODO: Load data from text file");
+        App.setRoot("dashboard");
+    }
+
+}
