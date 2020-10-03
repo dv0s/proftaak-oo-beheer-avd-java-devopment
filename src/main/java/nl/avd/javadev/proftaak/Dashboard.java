42981166228b9ef496fdf5af36165bff64fa5965
Vincent Weltje <vincent.weltje@weareyou.com>
2020-10-03 15:11:13 +0200

    Create dashboard

--- src/main/java/nl/avd/javadev/proftaak/Dashboard.java
+++ src/main/java/nl/avd/javadev/proftaak/Dashboard.java
@@ -3,7 +3,6 @@ package nl.avd.javadev.proftaak;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,19 +10,18 @@ import javafx.scene.control.ComboBox;
 
 public class Dashboard implements Initializable {
 
-    private String[] shapes = {"Cone", "Cube", "Cylinder", "Prism", "Pyramid", "Sphere"};
-
     @FXML
-    private ComboBox<String> shapeOptions;
+    private ComboBox<String> shapesDropdown;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        this.initShapeOptionsDropdown();
+        this.initShapesDropdown();
     }
 
-    private void initShapeOptionsDropdown() {
-        shapeOptions.getItems().setAll(this.shapes);
-        shapeOptions.getSelectionModel().selectedItemProperty().addListener((selected, oldValue, newValue) -> {
+    private void initShapesDropdown() {
+        System.out.println(ShapeType.shapeNames);
+        shapesDropdown.getItems().setAll(ShapeType.shapeNames);
+        shapesDropdown.getSelectionModel().selectedItemProperty().addListener((selected, oldValue, newValue) -> {
             try {
                 handleShapeOptionsChange(newValue);
             } catch (IOException e) {
@@ -33,18 +31,10 @@ public class Dashboard implements Initializable {
     }
 
     private void handleShapeOptionsChange(String newValue) throws IOException {
-        if (newValue.equals("Cone")) {
-            App.setRoot("shapes/cone");
-        } else if (newValue.equals("Cube")) {
-            App.setRoot("shapes/cube");
-        } else if (newValue.equals("Cylinder")) {
-            App.setRoot("shapes/cylinder");
-        } else if (newValue.equals("Prism")) {
-            App.setRoot("shapes/prism");
-        } else if (newValue.equals("Pyramid")) {
-            App.setRoot("shapes/pyramid");
-        } else if (newValue.equals("Sphere")) {
-            App.setRoot("shapes/sphere");
+        for (String shapeName : ShapeType.shapeNames) {
+            if (shapeName.equals(newValue)) {
+                App.setRoot("shapes/" + shapeName);
+            }
         }
     }
 
