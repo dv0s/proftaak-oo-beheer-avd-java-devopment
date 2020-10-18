bb9e1d6281ea08b8a759216f7303589d8262e3b3
Danny <dannvanos93@gmail.com>
2020-10-18 20:17:27 +0200

    Cube test finished

--- src/main/java/nl/avd/javadev/proftaak/shapes/Cube.java
+++ src/main/java/nl/avd/javadev/proftaak/shapes/Cube.java
@@ -4,6 +4,7 @@ import nl.avd.javadev.proftaak.Shape;
 import nl.avd.javadev.proftaak.ShapeType;
 import nl.avd.javadev.proftaak.Calculable;
 
+import java.util.InputMismatchException;
 import java.util.Map;
 
 public class Cube extends Shape implements Calculable {
@@ -20,6 +21,15 @@ public class Cube extends Shape implements Calculable {
 
     @Override
     public double getVolume() throws NumberFormatException {
+        if (this.getProperty("length") < 0){
+            throw new InputMismatchException("Length cannot be lower than 0");
+        }
+        if (this.getProperty("width") < 0){
+            throw new InputMismatchException("Width cannot be lower than 0");
+        }
+        if (this.getProperty("height") < 0){
+            throw new InputMismatchException("Height cannot be lower than 0");
+        }
         return this.getProperty("length") * this.getProperty("width") * this.getProperty("height");
     }
 
--- src/main/test/nl/avd/javadev/proftaak/shapes/CubeTest.java
+++ src/main/test/nl/avd/javadev/proftaak/shapes/CubeTest.java
@@ -1,15 +1,18 @@
 package nl.avd.javadev.proftaak.shapes;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 class CubeTest {
 
     @Test
+    @DisplayName("Return 1000 when values are 10, 10 and 10")
     void getVolume_for10and10and10_expect1000() {
         // Arrange
         Map<String, String> properties = new HashMap<>();
@@ -28,26 +31,43 @@ class CubeTest {
         assertEquals(1000, actual);
     }
 
-//    @Test
-//    void getVolume_throwsExceptionOn() {
-//        // Arrange
-//        Map<String, String> properties = new HashMap<>();
-//        properties.put("length" , "10");
-//        properties.put("width" , "andere tekst");
-//        properties.put("height" , "pannekoek");
-//
-//        Cube cube = new Cube(null);
-//
-//        cube.setProperties(properties);
-//
-//        // Act
-//        double actual = cube.getVolume();
-//
-//        // Assert
-//        assertThrows(NumberFormatException.class, cube.getVolume());
-//    }
+    @Test
+    void getVolume_forNegativeValues_returnException(){
+        // Arrange
+
+        // Act
+
+        // Assert
+        assertThrows(InputMismatchException.class, () -> {
+            Map<String, String> properties = new HashMap<>();
+            properties.put("length" , "-10");
+            properties.put("width" , "-10");
+            properties.put("height" , "-10");
+
+            Cube cube = new Cube(null);
+
+            cube.setProperties(properties);
+            cube.getVolume();
+        });
+
+    }
 
     @Test
-    void calculateVolume() {
+    @DisplayName("Throw exception when a String has been given.")
+    void getVolume_throwsExceptionOn() {
+        // Arrange
+        // Act
+        // Assert
+        assertThrows(NumberFormatException.class, () -> {
+            Map<String, String> properties = new HashMap<>();
+            properties.put("length" , "10");
+            properties.put("width" , "andere tekst");
+            properties.put("height" , "pannekoek");
+
+            Cube cube = new Cube(null);
+
+            cube.setProperties(properties);
+            cube.getVolume();
+        });
     }
 }
