e2ee8ca778fa48cf440a42958910415db8195cef
Danny <dannvanos93@gmail.com>
2020-10-19 19:33:18 +0200

    Added Prism Test and functionality

--- src/main/java/nl/avd/javadev/proftaak/shapes/Prism.java
+++ src/main/java/nl/avd/javadev/proftaak/shapes/Prism.java
@@ -4,12 +4,14 @@ import nl.avd.javadev.proftaak.Shape;
 import nl.avd.javadev.proftaak.ShapeType;
 import nl.avd.javadev.proftaak.Calculable;
 
+import java.util.InputMismatchException;
+
 public class Prism  extends Shape implements Calculable {
 
     public Prism(Integer id) {
         super();
         this.type = ShapeType.PRISM;
-        this.fields = new String[]{"length", "height"};
+        this.fields = new String[]{"triangleBase", "triangleHeight", "prismHeight"};
         this.id = id;
         if (id != null) {
             this.getDataFromDatabase();
@@ -17,8 +19,18 @@ public class Prism  extends Shape implements Calculable {
     }
 
     @Override
-    public double getVolume() {
-        return 0.0;
+    public double getVolume() throws NumberFormatException, InputMismatchException {
+        if (this.getProperty("triangleBase") < 0){
+            throw new InputMismatchException("triangle base cannot be negative");
+        }
+        if (this.getProperty("triangleHeight") < 0){
+            throw new InputMismatchException("Triangle height cannot be negative");
+        }
+        if (this.getProperty("prismHeight") < 0){
+            throw new InputMismatchException("Prism height cannot be negative");
+        }
+
+        return ((0.5) * (this.getProperty("triangleBase") * this.getProperty("triangleHeight"))) * this.getProperty("prismHeight");
     }
 
     @Override
---
+++ src/main/test/nl/avd/javadev/proftaak/shapes/PrismTest.java
@@ -0,0 +1,74 @@
+package nl.avd.javadev.proftaak.shapes;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PrismTest {
+    private static Map<String, String> properties;
+    private static Prism sut;
+
+    @BeforeAll
+    static void beforeAll(){
+        properties = new HashMap<>();
+        sut = new Prism(null);
+    }
+
+    @Test
+    @DisplayName("Return 70 when values are 4 and 5 for the triangle, and 7 for the height of the Prism")
+    void getVolume_for10and10_expect1000() {
+        // Arrange
+        properties.put("triangleBase" , "4");
+        properties.put("triangleHeight" , "5");
+        properties.put("prismHeight", "7");
+        sut.setProperties(properties);
+
+        // Act
+        double actual = sut.getVolume();
+
+        // Assert
+        assertEquals(70.00, actual, 0.01);
+    }
+
+    @Test
+    @DisplayName("Return NumberFormatException when values are garbage")
+    void getVolume_forGarbage_returnGarbage() {
+        // Arrange
+        properties.put("triangleBase" , "4");
+        properties.put("triangleHeight" , "pannekoek");
+        properties.put("prismHeight", "7");
+        sut.setProperties(properties);
+
+        // Act
+
+        // Assert
+        assertThrows(NumberFormatException.class, () -> {
+            sut.getVolume();
+        });
+    }
+
+    @Test
+    @DisplayName("Don't allow negative values")
+    void getVolume_forNegativeValues_returnException(){
+        // Arrange
+        properties.put("triangleBase" , "4");
+        properties.put("triangleHeight" , "-5");
+        properties.put("prismHeight", "7");
+        sut.setProperties(properties);
+
+        // Act
+
+        // Assert
+        assertThrows(InputMismatchException.class, () -> {
+            sut.getVolume();
+        });
+
+    }
+}
