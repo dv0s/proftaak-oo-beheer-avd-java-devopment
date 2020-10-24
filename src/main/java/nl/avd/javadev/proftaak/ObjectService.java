f5a3ac7b61b2805d096eca432e73357b4ce1a87f
Danny <dannvanos93@gmail.com>
2020-10-24 16:47:34 +0200

    Added ObjectIO to project

---
+++ src/main/java/nl/avd/javadev/proftaak/ObjectService.java
@@ -0,0 +1,41 @@
+package nl.avd.javadev.proftaak;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ObjectService {
+
+    private final String filename;
+
+    public ObjectService() {
+        this.filename = "shapes.dump.obj";
+    }
+
+    public void writeShapes(List<Shape> shapes) {
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(this.filename))) {
+            objectOutputStream.writeObject(shapes);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Shapes readShapes() {
+        Shapes shapes = null;
+
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(this.filename))) {
+            shapes = (Shapes) objectInputStream.readObject();
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return shapes;
+    }
+
+}
